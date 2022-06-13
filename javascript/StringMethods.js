417b7099700ea5f16acd0f0f755982a47c803a7b
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-06-13 18:25:19 +0200

    updated

--- javascript/StringMethods.js
+++ javascript/StringMethods.js
@@ -1,6 +1,6 @@
 // string methods
 var result;
-const MyText = 'lorem ipsum dolor sit amet intecbrussel sdjsidisjdijsidjsdj';
+const MyText = 'dolor ipsum brussel sit ipsum amet';
 
 // length
 result = MyText.length;
@@ -14,6 +14,40 @@ var firstname = "John "
 var lastname = "Doe"
 result = firstname.concat(lastname); // return JohnDoe
 result = "1234".concat("5678"); // return 12345678
+
+// startwidth() method and endwidth() method
+result = MyText.endsWith("intec") // true or false
+result = MyText.startsWith("lorem")
+
+// includes method
+result = MyText.includes("ipsum")
+
+// indexOf() method
+result = MyText.indexOf("brussel") // return index of word 
+
+// replace method
+//result = MyText.replace("ipsum","javascript")
+result = MyText.replaceAll("ipsum","javascript");
+
+// split method
+var names="Ersin,Hande,Alex,John,Jane";
+result = MyText.split(" ");
+result = names.split(",")
+
+// slice method 
+result = MyText.slice(0,MyText.length-4)
+
+result = MyText.toUpperCase(); // return all characters in uppercase
+result = MyText.toLocaleLowerCase(); // return all characters in lowercase
+
+var MyText2 = "             lorem                    ipsum             ";
+//result = MyText2.trim().slice(5,MyText2.length-32); // return lorem ipsum
+result = MyText2.trim().slice(0,6).concat(MyText2.trim().slice(MyText2.length-31)); // return lorem ipsum
+//console.log(MyText2)
+var MyNewText= "dolor ipsum dolor brussel BELGIUM sit dolor brussel ipsum amet 123456789"
+result = MyNewText.match(/brussel/)
+result = MyNewText.match(/brussel/g)
+
 
 
 console.log(result)
