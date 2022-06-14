c49208ab09b6ed33dd8302a8b5a3e7ce379f21fe
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-06-14 13:54:13 +0200

    filterpost exercise

--- javascript/arrayMethods.js
+++ javascript/arrayMethods.js
@@ -1,7 +1,7 @@
 // array methods
 var result;
 var myArr = [];
-var myNumbers = [5, 6, 7, 2, 9, 33, 100,15,10, 66, 11, 99, 77]
+var myNumbers = [5, 6, 7, 2, 9, 33, 100, 15, 10, 66, 11, 99, 77]
 var myLetters = ['c', 'b', 'a', 'd', 'e', 'g', 'f', 'h', 'j', 'i']
 // push method 
 myArr.push(1)
@@ -33,7 +33,7 @@ result = myNumbers.concat(myLetters) // combines two arrays
 //     console.log(result[i])
 // }
 
-let emptyArray=[4,6,'atilla','intec','koc']
+let emptyArray = [4, 6, 'atilla', 'intec', 'koc']
 function NewForLoop(value, index, arrayitself) {
     console.log(value, index, arrayitself)
 }
@@ -43,20 +43,65 @@ function NewForLoop(value, index, arrayitself) {
 //     console.log(emptyArray[i],emptyArray.length)
 // }
 
-var total=0
+var total = 0
 // for(let i=0;i<myNumbers.length;i++){
 //     //console.log(myNumbers[i])
 //     total+=myNumbers[i]
 //     console.log(total)
 // }
 
-myNumbers.forEach((n)=>total+=n)
+myNumbers.forEach((n) => total += n)
 result = `Total result is ${total}`
 
 // result.map(NewForLoop)
 
 
+// filter method
 
+function FilterCallBack(value) {
+    return value >= 30
+}
 
+let posts = [{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+},
+{
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+},
+{
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+},
+{
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+},
+{
+    "userId": 1,
+    "id": 5,
+    "title": "nesciunt quas odio",
+    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+}]
+
+result = myNumbers.filter(value => value >= 30)
+// result = posts.filter(value=>{
+//    return value.id === 5
+// })
+
+function FindPostById(id){
+    return posts.filter(value=>value.id===id)
+}
+
+result = FindPostById(5)
 
 console.log(result)
