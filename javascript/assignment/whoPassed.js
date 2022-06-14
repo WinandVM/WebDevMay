b5efdd6c326c19ee4022ea39b5a798f5d67a9333
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-06-14 14:08:34 +0200

    updated

---
+++ javascript/assignment/whoPassed.js
@@ -0,0 +1,26 @@
+var _students = [
+    {
+        id:1,
+        name:'avarel',
+        course:'javascript',
+        score:45
+    },
+    {
+        id:1,
+        name:'mike',
+        course:'java',
+        score:55
+    },
+    {
+        id:1,
+        name:'george',
+        course:'javascript',
+        score:66
+    },
+    {
+        id:1,
+        name:'joe',
+        course:'python',
+        score:70
+    }
+]
