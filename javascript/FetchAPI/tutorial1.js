d70fb5f6bf947053ef32d94f8bdc99e72b474a42
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-06-28 11:13:33 +0200

    fetch api

--- javascript/FetchAPI/tutorial1.js
+++ javascript/FetchAPI/tutorial1.js
@@ -33,13 +33,50 @@
 // })
 // .catch(err=>console.log(err))
 
-fetch('http://localhost:5000/api')
-.then(res=>res.json())
-.then(data=>data)
-.then(data=>{
-    data.map(user=>{
-        user.username = user.username.toUpperCase() // 'atilla' = 'atilla'.toUpperCase()
+// fetch('http://localhost:5000/api')
+// .then(res=>res.json())
+// .then(data=>data)
+// .then(data=>{
+//     data.map(user=>{
+//         user.username = user.username.toUpperCase() // 'atilla' = 'atilla'.toUpperCase()
+//     })
+//     return data
+// })
+// .then(data=>console.log(data))
+
+// async function getUsers(){
+//     let response = await fetch('http://localhost:5000/api')
+//     return response.json()
+// }
+
+// getUsers()
+// .then(data=>{
+//     console.log(data)
+// })
+
+// select tbody element.
+const TbodyResult = document.getElementById('result')
+
+// fetching data from the server
+let getUsers = new Promise((resolve,reject)=>{
+    fetch('http://localhost:5000/api')
+    .then(res=>res.json())
+    .then(data=>{
+        resolve(data)
     })
-    return data
+    .catch(err=>reject(err))
 })
-.then(data=>console.log(data))
+
+// get JSON data
+getUsers
+.then(data=>{
+    // iterate through the data
+    data.forEach(user=>{
+        // print the data in the table in html document.
+        TbodyResult.innerHTML += `<tr>
+        <td>${user.id}</td>
+        <td>${user.username}</td>
+        <td>${user.password}</td>
+    </tr>` 
+    })
+})
