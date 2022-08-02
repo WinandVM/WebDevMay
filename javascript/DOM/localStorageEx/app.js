46a1f2805c024eb12f431786e379d55938a32bde
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-02 11:43:24 +0200

    updated

--- javascript/DOM/localStorageEx/app.js
+++ javascript/DOM/localStorageEx/app.js
@@ -3,25 +3,61 @@ const userInput = document.querySelector('.userinput')
 const saveBtn = document.querySelector('.savebtn')
 const getBtn = document.querySelector('.getbtn')
 
+// let user = prompt('Enter your username');
+let user;
+// setTimeout(()=>{
+//     user = prompt('Enter your username');
+// },3000)
+
+// setInterval(()=>{
+//     user = prompt('Enter your username');
+//     if(JSON.parse(localStorage.getItem('students')) !==null){
+//         let getStorageData = JSON.parse(localStorage.getItem('students')) // temp localstorage data
+//         getStorageData.push(user) // add new data to temp array
+//         localStorage.setItem('students',JSON.stringify(getStorageData)) // re-write array data into localstorage
+//     }else{
+//         let students = [] // create new array
+//         students.push(user) // add new data to array
+//         localStorage.setItem('students',JSON.stringify(students)) // re-write array data into localstorage
+//     }
+// },3000)
+
+document.addEventListener('contextmenu',
+    event => {
+    
+        
+    });
+    document.addEventListener('selectstart',
+    event => {
+        console.log('stop it!')
+        
+    });
+
+// let x = setInterval(()=>{
+//     alert('lol')
+// },Infinity)
+
 saveBtn.addEventListener('click', () => {
-    if(JSON.parse(localStorage.getItem('students')) !==null){
+    if (JSON.parse(localStorage.getItem('students')) !== null) {
         let getStorageData = JSON.parse(localStorage.getItem('students')) // temp localstorage data
         getStorageData.push(userInput.value) // add new data to temp array
-        localStorage.setItem('students',JSON.stringify(getStorageData)) // re-write array data into localstorage
-    }else{
+        localStorage.setItem('students', JSON.stringify(getStorageData)) // re-write array data into localstorage
+    } else {
         let students = [] // create new array
         students.push(userInput.value) // add new data to array
-        localStorage.setItem('students',JSON.stringify(students)) // re-write array data into localstorage
+        localStorage.setItem('students', JSON.stringify(students)) // re-write array data into localstorage
     }
-    
-   
+
+
 })
 
-getBtn.addEventListener('click',()=>{
+getBtn.addEventListener('click', () => {
     // let username = localStorage.getItem('username')
     // console.log(`Username is ${username}`)
     let studentNames = JSON.parse(localStorage.getItem('students'))
     console.log(studentNames)
+    console.log(user)
+    window.open('https://intecbrussel.be/','_blank','width=1000,height=1000')
 })
 
 // document.addEventListener('keyup',(e)=>{
@@ -30,3 +66,5 @@ getBtn.addEventListener('click',()=>{
 //     // localStorage.setItem('logs',logs)
 //     // console.log(localStorage.getItem('logs'))
 // })
+
+window.resizeTo(500,500)
