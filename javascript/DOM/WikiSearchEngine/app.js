e4a54e9af1637c48cfb7af4b19d1af7be9bfb9bb
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-08 11:27:02 +0200

    js events

--- javascript/DOM/WikiSearchEngine/app.js
+++ javascript/DOM/WikiSearchEngine/app.js
@@ -20,36 +20,110 @@ async function searchData(keyword, limit) {
                     
 */
 
-const g = t => document.createElement(t) // create element
-function HtmlTemplateGenerator(obj) {
-    const li = g('li')
-    li.className = "list-group-item d-flex justify-content-between align-items-start"
+// const g = t => document.createElement(t) // create element
+// function HtmlTemplateGenerator(obj) {
+//     const li = g('li')
+//     li.className = "list-group-item d-flex justify-content-between align-items-start"
 
-    const parentDiv = g('div')
-    parentDiv.className = "ms-2 me-auto"
-    const textOfParentDiv = document.createTextNode(obj.snippet)
-  
-
-    const childDiv = g('div')
-    childDiv.className = "fw-bold"
-    const textOfChildDiv = document.createTextNode(obj.title)
-    childDiv.append(textOfChildDiv)
-    parentDiv.append(childDiv)
-    parentDiv.append(textOfParentDiv)
-    li.append(parentDiv)
-    return ResultsSection.append(li)
-}
+//     const parentDiv = g('div')
+//     parentDiv.className = "ms-2 me-auto"
+//     const textOfParentDiv = document.createTextNode(obj.snippet)
 
 
-wikiSearch.addEventListener('click', (e) => {
-    // fetching data here
-    searchData(searchInput.value, 30)
-        .then(data => {
-            console.log(data.query.search) // retrieved data
-            data.query.search.forEach(obj=>{
-                HtmlTemplateGenerator(obj)
-            })
-        })
-    console.log('hello world', searchInput.value)
-    e.preventDefault()
+//     const childDiv = g('div')
+//     childDiv.className = "fw-bold"
+//     const textOfChildDiv = document.createTextNode(obj.title)
+//     childDiv.append(textOfChildDiv)
+//     parentDiv.append(childDiv)
+//     parentDiv.append(textOfParentDiv)
+//     li.append(parentDiv)
+//     return ResultsSection.append(li)
+// }
+
+
+// wikiSearch.addEventListener('click', (e) => {
+//     ResultsSection.innerHTML='' // clear the results section
+//     // fetching data here
+//     searchData(searchInput.value, 30)
+//         .then(data => {
+//             console.log(data.query.search) // retrieved data
+//             data.query.search.forEach(obj=>{
+//                 HtmlTemplateGenerator(obj)
+//             })
+//         }).catch(err=>{
+//             alert('No data found')
+//         })
+//     console.log('hello world', searchInput.value)
+//     e.preventDefault()
+// })
+
+// html events
+// mouse events
+
+
+// mouseover
+ResultsSection.addEventListener('mouseover', (e) => {
+    console.log('Mouse Over Event', e.target)
+    if (e.target.classList.contains('bg-dark')) {
+        e.target.classList.remove('bg-dark')
+        e.target.classList.add('bg-danger')
+    }
+
+})
+
+// mouseout
+ResultsSection.addEventListener('mouseout', (e) => {
+    console.log('Mouse Out Event', e.target)
+    e.target.classList.add('bg-dark')
+    e.target.classList.add('text-light')
+})
+
+// mousedown
+ResultsSection.addEventListener('mousedown', (e) => {
+    console.log('Mouse down event', e.target)
+})
+
+// mouseup
+ResultsSection.addEventListener('mouseup', (e) => {
+    console.log('Mouse up event', e.target)
+})
+
+// keyboard events
+
+// keydown // works all keypresses
+// searchInput.addEventListener('keydown',(e)=>{
+//     console.log('Key Down Event', e.key)
+// })
+
+// keyup // works all keypresses
+// searchInput.addEventListener('keyup',(e)=>{
+//     console.log('Key Up Event', e.key)
+// }) 
+
+// keypress only works for chars
+// searchInput.addEventListener('keypress',(e)=>{
+//     console.log('Key press Event', e.key)
+// });
+
+// change event
+searchInput.addEventListener('change', (e) => {
+    console.log('Change Event', e.target.value)
+})
+
+// focus event
+searchInput.addEventListener('focus', (e) => {
+    console.log('Focus Event', e.target.value)
+    e.target.classList.add('bg-dark')
+    e.target.classList.add('text-light')
+})
+
+searchInput.addEventListener('blur', (e) => {
+    console.log('Blur Event', e.target.value)
+    if (e.target.classList.contains('bg-dark')) {
+        e.target.classList.remove('bg-dark')
+        e.target.classList.remove('text-light')
+        e.target.classList.add('bg-light')
+        e.target.classList.add('text-dark')
+    }
+
 })
