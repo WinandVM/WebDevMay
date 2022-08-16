cf11eb2efa588d64a3a5a67fa919a26c5e5d809b
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-16 13:46:01 +0200

    React.js intro

---
+++ reactjs/intro/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import ChildComponent from './ChildComponent';
+/*
+Classes
+Arrow Functions
+Variables (let, const, var)
+Array Methods like .map()
+Destructuring
+Modules
+Ternary Operator
+Spread Operator
+*/
+
+
+function App() {
+  return (
+   <div>
+     <ChildComponent/>
+    <h1>Hello world</h1>
+   </div>
+  );
+}
+
+export default App;
