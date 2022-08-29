f90e67721b0cb2fdc2f5627c2526263eb38cc1d4
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-29 09:45:02 +0200

    updated

--- reactjs/reactjs-layout/src/components/Homepage/MovieCard.js
+++ reactjs/reactjs-layout/src/components/Homepage/MovieCard.js
@@ -3,10 +3,10 @@ import { Card } from 'react-bootstrap'
 
 function MovieCard({movie,test}) {
     return (
-        <li key="1" id="movie1" data-toggle="modal" data-target="#moviedetail">
+        <li key={movie.id} id="movie1" data-toggle="modal" data-target="#moviedetail">
         <div className="movie">
           <figure className="movie__figure">
-          <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg" className="movie__poster"/>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="movie__poster"/>
             <figcaption><span className="movie__vote">4.5</span></figcaption>
             <h2 className="movie__title">Godzilla vs. Kong</h2>
           </figure>
