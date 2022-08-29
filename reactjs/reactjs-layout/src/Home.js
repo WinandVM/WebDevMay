070f174178299b84fc109125fffa07f2fe99dc99
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-29 09:18:45 +0200

    updated

--- reactjs/reactjs-layout/src/Home.js
+++ reactjs/reactjs-layout/src/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { CardGroup, Container } from 'react-bootstrap'
 import CarouselComponent from './components/Homepage/CarouselComponent'
+import MovieCard from './components/Homepage/MovieCard'
 
 function Home() {
+  const [movies,setMovies] = useState([])
+
+  const fetchMovies = async ()=>{
+    let response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US&page=1')
+    let data = await response.json()
+    setMovies(data.results)
+  }
+
+  useEffect(()=>{
+    fetchMovies()
+  },[])
+  
   return (
-    <Container fluid={true} className="p-0">
+    <Container fluid={true} className="">
       <CarouselComponent />
+      {
+        console.log(movies)
+      }
+      <ul className="movies">
+        {
+          movies !=undefined?
+          movies.map((movie,index)=>(
+            <MovieCard movie={movie} test={'this is a test props data'}/>
+          ))
+          :
+          ''
+        }
+      </ul>
     </Container>
   )
 }
