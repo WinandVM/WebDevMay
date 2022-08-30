45718d378b292861fd0f7cfd0a85916ba909542a
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-08-30 11:13:52 +0200

    moviecms pagination

--- reactjs/reactjs-layout/src/Home.js
+++ reactjs/reactjs-layout/src/Home.js
@@ -1,39 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import { CardGroup, Container } from 'react-bootstrap'
+import { Button, ButtonGroup, CardGroup, Container } from 'react-bootstrap'
 import CarouselComponent from './components/Homepage/CarouselComponent'
 import MovieCard from './components/Homepage/MovieCard'
 function Home() {
-  const [movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([])
+  const [page,setPage] = useState(1)
 
-  const fetchMovies = async ()=>{
-    let response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US&page=1')
+  // clickhandlers for pagination
+  const nextPage = ()=>setPage(page+1)
+  const prevPage = ()=>{
+    if(page <=1){
+      return
+    }else{
+      setPage(page-1)
+    }
+  }
+
+  const fetchMovies = async () => {
+    let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US&page=${page}`)
     let data = await response.json()
     setMovies(data.results)
   }
-
-  useEffect(()=>{
+  // https://api.themoviedb.org/3/movie/popular?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US&page=1
+  useEffect(() => {
     fetchMovies()
-    
-  },[])
-  
+
+  }, [page])
+
   return (
     <>
-    <Container fluid={true} className="p-0">
-      <CarouselComponent />
-      {
-        console.log(movies)
-      }
-    </Container>
-    <ul className="movies p-4 bg-dark">
+      <Container fluid={true} className="p-0">
+        <CarouselComponent />
         {
-          movies !=undefined?
-          movies.map((movie,index)=>(
-            <MovieCard movie={movie} test={'this is a test props data'}/>
-          ))
-          :
-          ''
+          console.log(page,"Page number")
+        }
+      </Container>
+      <ul className="movies p-4 bg-dark">
+        {
+          movies != undefined ?
+            movies.map((movie, index) => (
+              <MovieCard movie={movie} key={index} test={'this is a test props data'} />
+            ))
+            :
+            ''
         }
       </ul>
+
+      <div className="pagination bg-dark d-flex justify-content-center pb-2">
+        <ButtonGroup aria-label="Basic example">
+          <Button variant="primary" onClick={prevPage} className="rounded-0">Previous</Button>
+          <Button variant="primary" onClick={nextPage} className="rounded-0">Next</Button>
+        </ButtonGroup>
+      </div>
     </>
   )
 }
--- reactjs/reactjs-layout/src/MovieDetail.js
+++ reactjs/reactjs-layout/src/MovieDetail.js
@@ -5,7 +5,9 @@ import { useParams } from 'react-router-dom'
 
 function MovieDetail() {
     const [movieDetail, setMovieDetail] = useState([])
+    const [movieTrailer,setMovieTrailer] = useState('')
     // https://api.themoviedb.org/3/movie/616037?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US
+    // https://api.themoviedb.org/3/movie/616037/videos?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US
     const { movie_id } = useParams()
     // async func 
     useEffect(() => {
@@ -14,6 +16,13 @@ function MovieDetail() {
             .then(data => {
                 setMovieDetail(data)
             })
+            // https://www.youtube.com/watch?v=oRu0KYKWEGQ
+        fetch(`https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=07a61de5b731a869bc9cec8e25d2c8a8&language=en-US`)
+        .then(response=>response.json())
+        .then(data=>{
+           setMovieTrailer(data.results[0].key)
+           // console.log(data)
+        })
     }, [movie_id])
     return (
         <Container className="bg-dark p-0" fluid={true}>
@@ -47,20 +56,20 @@ function MovieDetail() {
                                         ''
                                 }
                             </ListGroup>
-
+                                
                         </ListGroup.Item>
 
                         <ListGroup.Item className="bg-none text-light border-0 py-1">
                             <Badge bg="warning" className="w-100 rounded-0 text-start" text="dark">Production: </Badge>
                             <ListGroup>
                                 {
-                                    movieDetail.production_companies !== undefined?
-                                    movieDetail.production_companies.map((prod)=>(
-                                        <ListGroup.Item className="rounded-0 py-0" key={prod.id}>
-                                            <a href={movieDetail.homepage} className="text-decoration-none text-dark" target="_blank">{prod.name}</a>
-                                        </ListGroup.Item>
-                                    )):
-                                    ''
+                                    movieDetail.production_companies !== undefined ?
+                                        movieDetail.production_companies.map((prod) => (
+                                            <ListGroup.Item className="rounded-0 py-0" key={prod.id}>
+                                                <a href={movieDetail.homepage} className="text-decoration-none text-dark" target="_blank">{prod.name}</a>
+                                            </ListGroup.Item>
+                                        )) :
+                                        ''
                                 }
                             </ListGroup>
 
@@ -69,7 +78,16 @@ function MovieDetail() {
                     </ListGroup>
                 </Col>
                 <Col md={8}>
-                                <h1 className="text-light display-6 p-2 movie-story">Overview</h1>
+                    <h1 className="text-light display-6 p-2 movie-story">Overview</h1>
+                    <p className="text-light">
+                        {
+                            movieDetail.overview !== undefined ?
+                                movieDetail.overview
+                                :
+                                'not available'
+                        }
+                    </p>
+                    <iframe width={560} height={315} src={`https://www.youtube.com/embed/${movieTrailer}`} title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </Col>
             </Row>
         </Container>
