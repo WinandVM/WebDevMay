c284ed057b9bf988db92f993b69005b19f7fb6f2
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-09-26 10:41:09 +0200

    hello world

--- nodejs/expressjs/hello-world/index.js
+++ nodejs/expressjs/hello-world/index.js
@@ -1,9 +1,17 @@
 // import express module
 const express = require('express')
 const server = express()
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+// use bodyParser middleware
+server.use(bodyParser({
+    extended:true
+}))
 
 // homepage http get request endpoint
 server.get('/',(request,response)=>{
+    console.log(request.query)
     // display html
     //response.send('Hello World')
     // display json
@@ -11,10 +19,28 @@ server.get('/',(request,response)=>{
     //     id:1,
     //     username:'admin'
     // })
-    response.json([{id:1},{id:2}])
-
+    //response.json([{id:1},{id:2}])
+    
+    console.log(request.body)
+    response.json(request.query) // response with url queries
+    
+    // http://localhost:8080?username=admin&password=123
 })
 
+// params
+server.get('/:id',(request,response)=>{
+    console.log(request.params)
+    response.json({id:1,paramResult:request.params})
+})
+
+// post http request
+server.post('/',(req,res)=>{
+    console.log(req.body)
+    res.json({
+    username:req.body.username,
+    password:req.body.password
+    })
+})
 
 
 server.listen(8080,()=>{ // set port number 
